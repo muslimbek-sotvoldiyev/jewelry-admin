248ce62d25bbf9890480837e025e967f2346353f
muslimbek-sotvoldiyev <muslimbeksotvoldiyev581@gmail.com>
2025-09-18 15:55:29 +0500

    add layout

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+// app/[locale]/layout.tsx
+import { ReactNode } from 'react';
+
+export default function LocaleLayout({
+  children,
+  params,
+}: {
+  children: ReactNode;
+  params: { locale: string };
+}) {
+  return (
+    <html lang={params.locale}>
+      <body>{children}</body>
+    </html>
+  );
+}
