607372b344c28bdbd6a33b2d5d6bec973624b873
Abdurazzoq Abdusalomov <rayyan.dev@mail.ru>
2025-09-18 23:15:49 +0500

    BASE URL changed

--- src/lib/service/api.ts
+++ src/lib/service/api.ts
@@ -1,9 +1,9 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseQuery = fetchBaseQuery({
-  // baseUrl: "https://zargar.pythonanywhere.com/api/v1/",
+  baseUrl: "https://zargar.pythonanywhere.com/api/v1/",
   // baseUrl: "https://moved-steadily-bear.ngrok-free.app/api/v1/",
-  baseUrl: "http://127.0.0.1:8000/api/v1/",
+  // baseUrl: "http://127.0.0.1:8000/api/v1/",
   prepareHeaders: (headers) => {
     const token = localStorage.getItem("access");
     if (token) {
